f8e5f73214749622dc2ebd9d55dfbee6b9fb7ce5
nshishkin <69085430+nshishkin@users.noreply.github.com>
2022-06-04 14:13:31 +0300

    Add right and left border control for ball

--- Pong.py
+++ Pong.py
@@ -82,3 +82,9 @@ while True:
     if ball.ycor() < -290:
         ball.sety(-290)
         ball.dy *= -1
+    if ball.xcor() > 390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+    if ball.xcor() < -390:
+        ball.goto(0, 0)
+        ball.dx *= -1
