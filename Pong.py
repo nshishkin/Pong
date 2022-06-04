6b70a5ace2b3e9075250c1cdf8ff38c4fbef5d20
nshishkin <69085430+nshishkin@users.noreply.github.com>
2022-06-04 13:57:32 +0300

    Add Right paddle movement

--- Pong.py
+++ Pong.py
@@ -46,10 +46,24 @@ def paddle_a_down():
     paddle_a.sety(y)
 
 
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
+
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 20
+    paddle_b.sety(y)
+
+
 # Keyboard binding
 wn.listen()
 wn.onkeypress(paddle_a_up, 'w')
 wn.onkeypress(paddle_a_down, 's')
+wn.onkeypress(paddle_b_up, 'Up')
+wn.onkeypress(paddle_b_down, 'Down')
 
 # Main game loop
 while True:
