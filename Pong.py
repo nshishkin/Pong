1ed1eeb9235a8c8f6cec49c1f4d7b216d77224f2
nshishkin <69085430+nshishkin@users.noreply.github.com>
2022-06-04 13:29:05 +0300

    Create Pong.py
    Create main runfile and add basic game loop

---
+++ Pong.py
@@ -0,0 +1,11 @@
+import turtle
+
+wn = turtle.Screen()
+wn.title("Pong by Nikolay Shishkin")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+# Main game loop
+while True:
+    wn.update()
