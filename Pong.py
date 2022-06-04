f888af56ca00865c69f08a8c05e2dca62983ce8e
nshishkin <69085430+nshishkin@users.noreply.github.com>
2022-06-04 14:10:07 +0300

    add top and bottom borded ball control

--- Pong.py
+++ Pong.py
@@ -31,6 +31,8 @@ ball.shape('square')
 ball.color('white')
 ball.penup()
 ball.goto(0, 0)
+ball.dx = 0.1
+ball.dy = 0.1
 
 
 # Function
@@ -68,3 +70,15 @@ wn.onkeypress(paddle_b_down, 'Down')
 # Main game loop
 while True:
     wn.update()
+
+    # Move the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    # Border checking
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
